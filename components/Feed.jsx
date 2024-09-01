d5a4a11c8ae6a3491ae7f6d52607e215ec7010a9
Gurudarshan57 <venom573116@gmail.com>
2024-09-01 18:12:47 +0530

    feat(frontend):add prompt card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,11 +1,15 @@
 'use client'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Image from 'next/image'
+import PromptCard from './PromptCard'
 
 const PromptCardList = ({ data, handleTagClick }) => {
     return (
-        <div>
-            {data?.map(ele => ele.prompt)}
+        <div className='px-2 space-y-6 py-8 sm:columns-2 sm:gap-6 xl:columns-3'>
+            {data?.map(ele =>
+                <PromptCard data={ele} key={ele._id} />
+            )}
         </div>
     )
 }
@@ -30,9 +34,9 @@ const Feed = () => {
 
     }
     return (
-        <div className='flex flex-col w-full border-2 pt-5'>
-            <form >
-                <input type="text" className='w-11/12 lg:w-2/4 p-1 rounded-sm outline-none px-3' placeholder='Search for a word, a tag or a username ...' />
+        <div className='flex flex-col w-full sm:w-4/5 pt-5 justify-center items-center'>
+            <form className='w-full'>
+                <input type="text" className='w-11/12 lg:w-3/5 h-10 p-1 rounded-sm outline-none px-3' placeholder='Search for a word, a tag or a username ...' />
             </form>
             <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
         </div>
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
 
-const PromptCard = () => {
+const PromptCard = ({ data }) => {
     return (
-        <div>PromptCard</div>
+        <div className="flex break-inside-avoid flex-col place-content-centerc p-5 rounded-md border-2 w-80 gap-2">
+            <div className="flex justify-between items-center">
+                <Image src={data.creator.image} width={40} height={40} className='rounded-full'></Image>
+                <div className="text-left">
+                    <p className="">{data.creator.username}</p>
+                    <p className="">{data.creator.email}</p>
+                </div>
+                <i className="fa-regular fa-copy text-sm p-2 border-2 rounded-full"></i>
+            </div>
+            <div className="text-justify">{data.prompt}</div>
+            <div className="text-left">{data.tag}</div>
+        </div>
     )
 }
 
