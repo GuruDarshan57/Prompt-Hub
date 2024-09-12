e679622ef61885e21506977a1006708cf39779c2
Gurudarshan57 <venom573116@gmail.com>
2024-09-12 08:10:25 +0530

    fix cache error

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,10 +1,12 @@
 import Prompts from "@models/prompt";
 import { connectToDB } from "@utils/database";
+import { revalidatePath } from "next/cache";
 
 export const GET = async (request) => {
     try {
         await connectToDB()
 
+        revalidatePath(request.url)
         const prompts = await Prompts.find({}).populate('creator')
 
         return new Response(JSON.stringify(prompts), { status: 200 })
